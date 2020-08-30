77abe0d01271af88bf2b7b677dfd7e6bb5660fb2
Jon McEwen <>
2020-08-29 19:51:04 -0500

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,42 +12,30 @@ import { faHandPointRight } from "@fortawesome/free-solid-svg-icons"
 import blog from "../images/typewriter.png"
 
 const IndexPage = () => {
-  const [isLoaded, setLoaded] = useState(false)
-
-  useLayoutEffect(() => {
-    setTimeout(() => {
-      setLoaded(true)
-    }, 1200)
-  })
-
-  if (isLoaded == true) {
-    return (
-      <>
-        <SEO title="Home" />
-        <div className="container">
-          <div className="hero">
-            <h1 className="jumbo">A Podcast for the Nerd Masses</h1>
-            <Link to="episodes">
-              <h3 className="episodes"> View Episodes 🍑🍆💦👀</h3>
-            </Link>
-            <img src={banner} className="banner" />
-          </div>
-          <div className="slideOne">
-            <div className="content">
-              <h1 className="whiteHeader">Latest Blog Posts</h1>
-              <div className="gridThree">
-                <Blog />
-                <Blog />
-                <Blog />
-              </div>
+  return (
+    <>
+      <SEO title="Home" />
+      <div className="container">
+        <div className="hero">
+          <h1 className="jumbo">A Podcast for the Nerd Masses</h1>
+          <Link to="episodes">
+            <h3 className="episodes"> View Episodes 🍑🍆💦👀</h3>
+          </Link>
+          <img src={banner} className="banner" />
+        </div>
+        <div className="slideOne">
+          <div className="content">
+            <h1 className="whiteHeader">Latest Blog Posts</h1>
+            <div className="gridThree">
+              <Blog />
+              <Blog />
+              <Blog />
             </div>
           </div>
         </div>
-      </>
-    )
-  } else if (isLoaded == false) {
-    return <Spinner />
-  }
+      </div>
+    </>
+  )
 }
 
 export default IndexPage
