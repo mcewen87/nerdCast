6e03735a22b537767cab8bcbfc6dbc3290e29448
Jon McEwen <>
2020-11-27 13:51:13 -0600

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,12 @@ import banner from "../images/nerdyBanner.png"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faHandPointRight } from "@fortawesome/free-solid-svg-icons"
 import Sponsors from "../components/sponsors"
+import {
+  faInstagram,
+  faTwitter,
+  faFacebook,
+} from "@fortawesome/free-brands-svg-icons"
+import style from "../components/styles/footer.module.css"
 
 const IndexPage = () => {
   return (
@@ -40,9 +46,37 @@ const IndexPage = () => {
             <p className="white">
               With the addition of a website, we are excited to give our
               listeners another outlet to enjoy our media. In the near future,
-              we plan on expanding our websites features to include a blog and
+              we plan on expanding our website's features to include a blog and
               online merchandise shop.
             </p>
+
+            <h2 className="red">The Community</h2>
+            <p className="white">
+              Get involved in the Holy Nerdy Community by following us on
+              Twitter, Facebook, and Instagram. You can also join our Discord
+              server to chat with fellow nerds. Don't miss our exclusive
+              giveaways and events.
+            </p>
+            <div className="row">
+              <a
+                target="_blank"
+                href="https://www.instagram.com/holynerdypod/?hl=en"
+              >
+                <FontAwesomeIcon className={style.icon} icon={faInstagram} />
+              </a>
+              <a
+                target="_blank"
+                href="https://twitter.com/holynerdypod?lang=en"
+              >
+                <FontAwesomeIcon className={style.icon} icon={faTwitter} />
+              </a>
+              <a
+                target="_blank"
+                href="https://www.facebook.com/HolyNerdyPodcast"
+              >
+                <FontAwesomeIcon className={style.icon} icon={faFacebook} />
+              </a>
+            </div>
           </div>
         </div>
         <div className="slideThree">
